4c62b4e32b4295582b5853eee4de364686285b86
Andrew Raymer <andrewraymer@gmail.com>
2018-03-25 23:16:52 -0400

    Moved Hotjar to turbolinks ready format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+document.addEventListener('turbolinks:load', function(event) {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:826770,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+});
